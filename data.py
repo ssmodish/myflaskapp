e644b7216afec45e7d0490e4362722d23347e2fe
Steve Smodish <smodish@hotmail.com>
2019-10-28 19:14:27 -0400

    init commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Spicy jalapeno bacon ipsum dolor amet porchetta salami picanha deserunt eiusmod velit. Aliqua tri-tip fatback, kevin proident dolor short loin spare ribs est brisket. Shoulder rump consectetur ea. Minim duis sunt, proident porchetta id pork loin filet mignon jerky esse ea consectetur tenderloin beef.',
+            'author': 'that guy',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Spicy jalapeno bacon ipsum dolor amet porchetta salami picanha deserunt eiusmod velit. Aliqua tri-tip fatback, kevin proident dolor short loin spare ribs est brisket. Shoulder rump consectetur ea. Minim duis sunt, proident porchetta id pork loin filet mignon jerky esse ea consectetur tenderloin beef.',
+            'author': 'some other guy',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Spicy jalapeno bacon ipsum dolor amet porchetta salami picanha deserunt eiusmod velit. Aliqua tri-tip fatback, kevin proident dolor short loin spare ribs est brisket. Shoulder rump consectetur ea. Minim duis sunt, proident porchetta id pork loin filet mignon jerky esse ea consectetur tenderloin beef.',
+            'author': 'that guy',
+            'create_date': '04-25-2017'
+        },
+    ]
+
+    return articles
